6788654aa52fe3ebc20edab087f68b3bd8498f13
Sundus <sundusaden105@gmail.com>
2023-02-13 15:50:10 +0300

    completed

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,16 @@
 class Dog
-
+# setter method
+  def name=(name)
+    @name= name
+  end
+  def name
+    @name
+    end
+    
+  def breed=(new_breed)
+    @breed = new_breed
+  end
+  def breed
+    @breed
+  end
 end
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,15 @@
 class Person
-
+def name=person_name 
+    @name = person_name
+    end
+    def name
+        @name
+    end
+    def job=job
+        @job = job
+    end
+    def job
+        @job
+    end
 end
+
